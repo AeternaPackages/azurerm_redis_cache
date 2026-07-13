b98eadb9d8af54772fdae5faace6eef233bc3fb7
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:41:27 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -56,12 +56,12 @@ EOT
     replicas_per_primary               = optional(number)
     replicas_per_master                = optional(number)
     private_static_ip_address          = optional(string)
-    public_network_access_enabled      = optional(bool) # Default: true
+    public_network_access_enabled      = optional(bool)
     tenant_settings                    = optional(map(string))
-    non_ssl_port_enabled               = optional(bool)   # Default: false
-    minimum_tls_version                = optional(string) # Default: "1.2"
-    access_keys_authentication_enabled = optional(bool)   # Default: true
-    redis_version                      = optional(string) # Default: "6"
+    non_ssl_port_enabled               = optional(bool)
+    minimum_tls_version                = optional(string)
+    access_keys_authentication_enabled = optional(bool)
+    redis_version                      = optional(string)
     zones                              = optional(set(string))
     identity = optional(object({
       identity_ids = optional(set(string))
@@ -69,7 +69,7 @@ EOT
     }))
     patch_schedule = optional(list(object({
       day_of_week        = string
-      maintenance_window = optional(string) # Default: "PT5H"
+      maintenance_window = optional(string)
       start_hour_utc     = optional(number)
     })))
     redis_configuration = optional(object({
@@ -77,11 +77,11 @@ EOT
       aof_backup_enabled                      = optional(bool)
       aof_storage_connection_string_0         = optional(string)
       aof_storage_connection_string_1         = optional(string)
-      authentication_enabled                  = optional(bool) # Default: true
+      authentication_enabled                  = optional(bool)
       data_persistence_authentication_method  = optional(string)
       maxfragmentationmemory_reserved         = optional(number)
       maxmemory_delta                         = optional(number)
-      maxmemory_policy                        = optional(string) # Default: "volatile-lru"
+      maxmemory_policy                        = optional(string)
       maxmemory_reserved                      = optional(number)
       notify_keyspace_events                  = optional(string)
       rdb_backup_enabled                      = optional(bool)
